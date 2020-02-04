29f2ddd7620018dd68421ae986fce245120413aa
JeMor <1839750567@qq.com>
2020-02-04 16:18:43 +0800

    初始项目

--- mianshi.app/src/main/java/com/study/app/AppApplication.java
+++ mianshi.app/src/main/java/com/study/app/AppApplication.java
@@ -9,6 +9,7 @@ public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
         System.out.println("--------------加载完成----------------");
+        System.out.println("--------------你看到了吗？----------------");
     }
 
 }
