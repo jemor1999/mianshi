12b8832220788cc516d31991eaf8077307202d89
JeMor <1839750567@qq.com>
2020-02-04 18:03:33 +0800

    初始项目

--- mianshi.app/src/main/java/com/study/app/AppApplication.java
+++ mianshi.app/src/main/java/com/study/app/AppApplication.java
@@ -9,15 +9,6 @@ public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
         System.out.println("--------------加载完成----------------");
-        System.out.println("--------------你看到了吗？----------------");
-        System.out.println("--------------你又看到了吗？----------------");
-        System.out.println("--------------你真的不能看到了吗？----------------");
-        System.out.println("可以看见了");
-        System.out.println("真的看不见嘛");
-        System.out.println("看到了就好");
-        System.out.println("不能被覆盖");
-        
-        System.out.println("我来看看冲突");
     }
 
 }
