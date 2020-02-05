10d9b6c2a8faff26486a8c1e02ed9ef647c5ba0d
JeMor <1839750567@qq.com>
2020-02-05 12:58:16 +0800

    初始项目

--- mianshi.app/src/main/java/com/study/app/AppApplication.java
+++ mianshi.app/src/main/java/com/study/app/AppApplication.java
@@ -10,15 +10,6 @@ public class AppApplication {
         SpringApplication.run(AppApplication.class, args);
         System.out.println("--------------加载完成----------------");
         System.out.println("--------------你看到了吗？----------------");
-        System.out.println("--------------你又看到了吗？----------------");
-        System.out.println("--------------你真的不能看到了吗？----------------");
-        System.out.println("可以看见了");
-        System.out.println("真的看不见嘛");
-        System.out.println("看到了就好");
-        System.out.println("不能被覆盖");
-        System.out.println("OK、今天先到这、准备吃饭");
-        System.out.println("看的见我的提交嘛？");
-        System.out.println("看的见我的提交嘛？");
     }
 
 }
